e5bd71b3cdad43294c6d8a02389bb8cd0c033aaa
aquaschematic <aqua.schematic@gmail.com>
2016-04-04 20:39:54 -0400

    creating relationships for holdings

--- db/migrate/20160405001550_create_holdings.rb
+++ db/migrate/20160405001550_create_holdings.rb
@@ -1,11 +1,13 @@
 class CreateHoldings < ActiveRecord::Migration
   def change
     create_table :holdings do |t|
-      t.integer :profolio_id
+      t.integer :portfolio_id
       t.integer :team_id
-      t.decimal :blue_prints
+      t.integer :blue_prints
 
       t.timestamps null: false
     end
+    add_index :holdings, :portfolio_id
+    add_index :holdings, :team_id
   end
 end
