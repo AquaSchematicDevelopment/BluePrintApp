28c00e60d00f5bcc56126e7ed52b0a6e447127b0
Jack Miller <jack.miller.xc@gmail.com>
2016-04-04 22:43:27 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
   get 'sport/new', to: 'sports#new', as: :new_sport
   post 'sport/new', to: 'sports#create'
   get 'sport/:id', to: 'sports#show', as: :show_sport
+  get 'sport/:id/edit', to: 'sports#show', as: :edit_sport
+  post 'sport/:id/edit', to: 'sport#update'
   get 'sport/:sport_id/addLeague', to: 'leagues#new', as: :add_league
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
