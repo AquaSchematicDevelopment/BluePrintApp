da4f42f3a62b42cee9cbbbd26e08507f9a5f58ed
aquaschematic <aqua.schematic@gmail.com>
2016-04-04 21:21:39 -0400

    Update 20160401150611_create_teams.rb

--- db/migrate/20160401150611_create_teams.rb
+++ db/migrate/20160401150611_create_teams.rb
@@ -2,10 +2,10 @@ class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
       t.string :name
-      t.integer :league_id
+      t.integer :season_id
 
       t.timestamps null: false
     end
-    add_index :teams, :league_id
+    add_index :teams, :season_id
   end
 end
