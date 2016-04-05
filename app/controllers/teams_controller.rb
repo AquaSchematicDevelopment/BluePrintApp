d14e03374c13cde383135aa2fcdbe8a1999be236
chasemerkel <chase.merkel@gmail.com>
2016-04-05 09:23:07 -0400

    Update teams_controller.rb
    Changes made to index method.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,7 +4,7 @@ class TeamsController < ApplicationController
   # GET /teams
   # GET /teams.json
   def index
-    @teams = Team.all
+    @teams = Team.where(season_id: params[:season_id]
   end
 
   # GET /teams/1
