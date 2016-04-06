ac8ff51bfa370fd8d8903999c28c65c5c9793ea8
aquaschematic <aqua.schematic@gmail.com>
2016-04-05 23:12:13 -0400

    adding some sample data
    have no idea if it works

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(name: 'admin', password: 'rubyserver', email: 'aqua.schematic@gmail.com', role: 'admin')
+
+nfl = Sport.create(name: 'nfl')
+nfl_devision = League.create(name: 'devision', sport: nfl)
+season_2016 = Season.create(name: '2016', league: nfl_devision)
+team1 = Team.create(name: 'team1', season: season_2016)
+team2 = Team.create(name: 'team1', season: season_2016)
+
+andrew = User.create(name: 'andrew', password: 'furmancs', email: 'aqua.schematic@gmail.com', role: 'player')
+
+andrew_no_profolio = Profolio.create(user: andrew, season: season_2016, funds: 200.15)
+Holding.create(profolio: andrew_no_profolio, team: team1, blue_prints: 100)
+Holding.create(profolio: andrew_no_profolio, team: team2, blue_prints: 20)
