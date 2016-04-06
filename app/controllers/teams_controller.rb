e9af170606eecc84d6630da71e5d1e3775e789a3
aquaschematic <aqua.schematic@gmail.com>
2016-04-05 22:16:20 -0400

    fixed paths

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -35,7 +35,7 @@ class TeamsController < ApplicationController
     else
       respond_to do |format|
         if @team.save
-          format.html { redirect_to @team, notice: 'Team was successfully created.' }
+          format.html { redirect_to show_team_path(@team), notice: 'Team was successfully created.' }
           format.json { render :show, status: :created, location: @team }
         else
           format.html { render :new }
@@ -54,7 +54,7 @@ class TeamsController < ApplicationController
     else
       respond_to do |format|
         if @team.update(team_params)
-          format.html { redirect_to @team, notice: 'Team was successfully updated.' }
+          format.html { redirect_to show_team_path(@team), notice: 'Team was successfully updated.' }
           format.json { render :show, status: :ok, location: @team }
         else
           format.html { render :edit }
@@ -67,9 +67,10 @@ class TeamsController < ApplicationController
   # DELETE /teams/1
   # DELETE /teams/1.json
   def destroy
+    season = @team.season
     @team.destroy
     respond_to do |format|
-      format.html { redirect_to teams_url, notice: 'Team was successfully destroyed.' }
+      format.html { redirect_to show_season_path(season), notice: 'Team was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
