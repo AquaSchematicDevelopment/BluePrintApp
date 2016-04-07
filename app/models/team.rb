a49029388924c7da6f3e0b4e2ef71f329eddd3bf
aquaschematic <aqua.schematic@gmail.com>
2016-04-06 23:03:22 -0400

    Update team.rb

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,10 +3,4 @@ class Team < ActiveRecord::Base
   has_many :holdings
   has_many :sell_requests
   has_many :transactions
-  
-  def safe_name
-    return name.split(' ')
-      .map { |word| word[0].capitalize + word[1..-1] }
-      .join('')
-  end
 end
