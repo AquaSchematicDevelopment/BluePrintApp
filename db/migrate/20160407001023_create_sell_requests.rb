d12ee4b75a051ca0b851ea2976574859f24636d7
aquaschematic <aqua.schematic@gmail.com>
2016-04-06 20:46:06 -0400

    added indexs

--- db/migrate/20160407001023_create_sell_requests.rb
+++ db/migrate/20160407001023_create_sell_requests.rb
@@ -1,10 +1,15 @@
 class CreateSellRequests < ActiveRecord::Migration
   def change
     create_table :sell_requests do |t|
+      t.integer :portfolio_id
+      t.integer :team_id
+      
       t.decimal :price
       t.integer :amount
 
       t.timestamps null: false
     end
+    add_index :sell_requests, :portfolio_id
+    add_index :sell_requests, :team_id
   end
 end
