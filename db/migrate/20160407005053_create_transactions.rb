d2ae2579a04854dfe2378f1a141be1661e29606f
aquaschematic <aqua.schematic@gmail.com>
2016-04-06 20:56:12 -0400

    added indexes

--- db/migrate/20160407005053_create_transactions.rb
+++ db/migrate/20160407005053_create_transactions.rb
@@ -1,10 +1,15 @@
 class CreateTransactions < ActiveRecord::Migration
   def change
     create_table :transactions do |t|
+      t.integer :from_portfolio
+      t.integer :to_portfolio
+      t.integer :team_id
+      
       t.decimal :price
       t.int :amount
 
       t.timestamps null: false
     end
+    add_index :transactions, :team_id
   end
 end
