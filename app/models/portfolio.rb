0985462f0b0c2e9f1509d02c9dea4f64da5b1a44
aquaschematic <aqua.schematic@gmail.com>
2016-04-11 20:35:21 -0400

    added transactions

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,6 @@ class Portfolio < ActiveRecord::Base
   belongs_to :season
   has_many :holdings
   has_many :sell_request
+  has_many :transactions, as: :seller
+  has_many :transactions, as: :buyer
 end
