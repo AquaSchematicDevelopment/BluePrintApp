3d59f29150506c88c103f6a4daddf4cc8493fadf
chasemerkel <chase.merkel@gmail.com>
2016-04-13 15:20:28 -0400

    Update portfolio.rb

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,7 +2,7 @@ class Portfolio < ActiveRecord::Base
   belongs_to :user
   belongs_to :season
   has_many :holdings
-  has_many :sell_request
+  has_many :sell_requests
   has_many :transactions, as: :seller
   has_many :transactions, as: :buyer
 end
