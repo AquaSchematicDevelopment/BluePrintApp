0ac75d04d0839e3ae11bb23c5ec5cc7150430224
aquaschematic <aqua.schematic@gmail.com>
2016-04-14 00:04:47 -0400

    Update holding.rb

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -2,12 +2,12 @@ class Holding < ActiveRecord::Base
   belongs_to :portfolio
   belongs_to :team
   
-  def available_blueprints
-    self.amount - total_blueprints_currently_being_sold
-  end
-  
   def total_blueprints_currently_being_sold
     sell_requests = SellRequest.where(team: self.team, portfolio: self.portfolio)
     offerings.inject(0) { |total, offering| total + offering.amount }
+  end
+  
+  def available_blueprints
+    self.amount - total_blueprints_currently_being_sold
   end
 end
