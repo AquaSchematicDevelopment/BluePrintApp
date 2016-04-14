751fef095eef677328e10c5e87ed683e1e119b02
aquaschematic <aqua.schematic@gmail.com>
2016-04-14 00:19:05 -0400

    Update holding.rb

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -8,6 +8,6 @@ class Holding < ActiveRecord::Base
   end
   
   def available_blueprints
-    self.amount - self.total_blueprints_currently_being_sold
+    self.blue_prints - self.total_blueprints_currently_being_sold
   end
 end
