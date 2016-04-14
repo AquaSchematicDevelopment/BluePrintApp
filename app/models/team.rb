70302515b8c9bbc5284d7ccdf9a084e903818227
aquaschematic <aqua.schematic@gmail.com>
2016-04-13 23:31:34 -0400

    Update team.rb

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,15 +5,7 @@ class Team < ActiveRecord::Base
   has_many :transactions
   
   def lowest_priced_sell_requests
-    self.sell_requests.inject([]) do |map, sell_request|
-        if map.empty? || map.first.price == sell_request.price
-          map[] = sell_request
-        elsif sell_request.price < map.first.price
-          map = [sell_request]
-        else
-          map
-        end
-      end
+    self.sell_requests.filter { |sell_request| sell_request.price == self.lowest_price }
   end
   
   def lowest_price
