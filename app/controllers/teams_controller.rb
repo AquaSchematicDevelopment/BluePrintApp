00a85ed618f8460101ecaada7df18475a0f0d187
aquaschematic <aqua.schematic@gmail.com>
2016-04-14 14:39:33 -0400

    Update teams_controller.rb

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,5 @@
 class TeamsController < ApplicationController
-  before_action :set_team, only: [:show, :edit, :update, :destroy]
+  before_action :set_team, only: [:show, :edit, :update, :destroy, :sell_request_index]
   before_action :redirect_non_user
   before_action :redirect_non_admin, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
