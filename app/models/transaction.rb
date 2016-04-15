02a814f0755088f3c0d716e63939c6440126cb6b
aquaschematic <aqua.schematic@gmail.com>
2016-04-15 12:26:22 -0400

    Update transaction.rb

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ActiveRecord::Base
   belongs_to :team
-  belongs_to :buyer
-  belongs_to :seller
+  belongs_to :buyer, :class_name => 'Portfolio', :foreign_key => 'buyer_id'
+  belongs_to :seller, :class_name => 'Portfolio', :foreign_key => 'seller_id'
 end
