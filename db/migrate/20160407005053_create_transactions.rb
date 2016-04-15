52f914c2ef00790292fe2d6d0f78f4b7a6a55be5
aquaschematic <aqua.schematic@gmail.com>
2016-04-15 12:01:09 -0400

    Update 20160407005053_create_transactions.rb

--- db/migrate/20160407005053_create_transactions.rb
+++ db/migrate/20160407005053_create_transactions.rb
@@ -1,8 +1,8 @@
 class CreateTransactions < ActiveRecord::Migration
   def change
     create_table :transactions do |t|
-      t.integer :from_portfolio
-      t.integer :to_portfolio
+      t.integer :buyer_id
+      t.integer :seller_id
       t.integer :team_id
       
       t.decimal :price
@@ -10,6 +10,8 @@ class CreateTransactions < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+    add_index :transactions, :seller_id
+    add_index :transactions, :buyer_id
     add_index :transactions, :team_id
   end
 end
