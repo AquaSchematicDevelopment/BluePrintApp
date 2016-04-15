074d41ca0cdff9d42bdfe467c87fe3befbdba291
aquaschematic <aqua.schematic@gmail.com>
2016-04-15 11:55:37 -0400

    Update portfolio.rb

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,6 +3,7 @@ class Portfolio < ActiveRecord::Base
   belongs_to :season
   has_many :holdings
   has_many :sell_requests
+  has_many :buy_requests
   has_many :transactions, as: :seller
   has_many :transactions, as: :buyer
 end
