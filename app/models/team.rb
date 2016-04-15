5f6b08364f040376f981c72a2773ae3eef958c61
aquaschematic <aqua.schematic@gmail.com>
2016-04-15 11:55:11 -0400

    Update team.rb

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,7 @@ class Team < ActiveRecord::Base
   belongs_to :season
   has_many :holdings
   has_many :sell_requests
+  has_many :buy_requests
   has_many :transactions
   
   def lowest_priced_sell_requests
