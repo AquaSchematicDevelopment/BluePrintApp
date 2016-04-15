e29cd31d81a682864fd3b604e38824a4febc50fe
aquaschematic <aqua.schematic@gmail.com>
2016-04-15 11:52:13 -0400

    Update 20160415154524_create_buy_requests.rb

--- db/migrate/20160415154524_create_buy_requests.rb
+++ db/migrate/20160415154524_create_buy_requests.rb
@@ -1,10 +1,15 @@
 class CreateBuyRequests < ActiveRecord::Migration
   def change
     create_table :buy_requests do |t|
+      t.integer :portfolio_id
+      t.integer :team_id
+      
       t.integer :amount
       t.decimal :price
 
       t.timestamps null: false
     end
+    add_index :buy_requests, :portfolio_id
+    add_index :buy_requests, :team_id
   end
 end
