cd20262e03117058c945b155d19da184b140a173
aquaschematic <aqua.schematic@gmail.com>
2016-04-20 16:25:16 -0400

    Update 20160401150611_create_teams.rb

--- db/migrate/20160401150611_create_teams.rb
+++ db/migrate/20160401150611_create_teams.rb
@@ -3,6 +3,7 @@ class CreateTeams < ActiveRecord::Migration
     create_table :teams do |t|
       t.string :name
       t.decimal :book_value, precision: 12, scale: 4
+      t.decimal :market_price, precision: 12, scale: 4
       t.integer :season_id
 
       t.timestamps null: false
