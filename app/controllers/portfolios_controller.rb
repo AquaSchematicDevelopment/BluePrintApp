dfdddba2ed1a806baf4ded6ad70cd9bc036dd895
aquaschematic <aqua.schematic@gmail.com>
2016-04-20 16:09:32 -0400

    Update portfolios_controller.rb

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,8 +16,6 @@ class PortfoliosController < ApplicationController
     @holdings = @portfolio.holdings.sort_by{|holding| holding.team.name}
     @sell_requests = @portfolio.sell_requests.sort do |a,b|
          by_team = a.team.name <=> b.team.name
-         #by_price = a.price <=> b.price if by_team == 0
-         #by_amount = a.amount<=> b.amount if by_price && by_price == 0
          
          if by_team == 0
            by_price = a.price <=> b.price
