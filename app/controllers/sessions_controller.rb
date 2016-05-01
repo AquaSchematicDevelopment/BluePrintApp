08c849cd68263d371a0da836bcdfc90c0f9b8c56
aquaschematic <aqua.schematic@gmail.com>
2016-05-01 15:27:24 -0400

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
   
+  before_action :redirect_non_player, only: [:set_season]
+  
   def new
   end
 
@@ -7,12 +9,17 @@ class SessionsController < ApplicationController
     user = User.find_by_name(params[:session][:user_name])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      session[:season] = nil
+      session[:season_id] = nil
       redirect_to root_url
     else
       @errors = ['User Name or Password was incorrect']
       render template: 'sessions/new'
     end
+  end
+  
+  def set_season
+    session[:season_id] = params[:id]
+    redirect_to show_portfolio_path
   end
 
   def destroy
