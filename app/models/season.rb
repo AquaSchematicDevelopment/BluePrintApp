db5585be7dc1bcdde99c380a560de278fa7f8f07
aquaschematic <aqua.schematic@gmail.com>
2016-05-01 13:39:55 -0400

    Update season.rb

--- app/models/season.rb
+++ app/models/season.rb
@@ -5,4 +5,8 @@ class Season < ActiveRecord::Base
   def self.statuses
     [:unpublished, :pre_ipo, :ipo, :post_ipo, :game_open, :game_closed]
   end
+  
+  def self.check_valid_status(status)
+    self.statuses.find(|obj| obj == status)
+  end
 end
