ae4194767eb751948f0017293653a11ec4ad850d
aquaschematic <aqua.schematic@gmail.com>
2016-05-03 21:35:12 -0400

    Update players_controller.rb

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -15,6 +15,7 @@ class PlayersController < ApplicationController
   end
 
   def edit
+    
   end
 
   def create
@@ -70,7 +71,11 @@ class PlayersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_player
       user = User.find(params[:id])
-      @player = user if user.is_player?
+      if user.is_player?
+        @player = user
+      else
+        redirect_to root_path
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
